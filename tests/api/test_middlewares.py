d5caee8635e67f117b01b14f1607ebcb97636d8b
Thomas Fernandez <thomas.fernandez@inokufu.com>
2024-09-27 16:41:22 +0000

    ✅(project) handle x-experience-api-version header
    Reject requests without the "X-Experience-API-Version" header or with an unsupported version by responding with a 400 Bad Request error including a short description of the problem. Include the "X-Experience-API-Version" header in every response.
    Improve xapi specification conformance tested via lrs-conformance-test-suite :
    - 9 tests that failed now pass,
    - 0 test that passed now fail,
    - a total of 591/1365 test passed.

---
+++ tests/api/test_middlewares.py
@@ -0,0 +1,66 @@
+"""Tests for middlewares of the Ralph API."""
+
+from datetime import datetime, timedelta
+
+import pytest
+from httpx import AsyncClient
+
+from ralph.api import app
+
+
+@pytest.mark.anyio
+async def test_x_experience_api_version_header(
+    insert_statements_and_monkeypatch_backend, basic_auth_credentials
+):
+    """
+    Test X-Experience-API-Version header is checked in request and included in response.
+    """
+    statements = [
+        {
+            "id": "be67b160-d958-4f51-b8b8-1892002dbac6",
+            "timestamp": (datetime.now() - timedelta(hours=1)).isoformat(),
+        },
+        {
+            "id": "72c81e98-1763-4730-8cfc-f5ab34f1bad2",
+            "timestamp": datetime.now().isoformat(),
+        },
+    ]
+
+    insert_statements_and_monkeypatch_backend(statements)
+
+    async with AsyncClient(
+        app=app,
+        base_url="http://testserver",
+        headers={"Authorization": f"Basic {basic_auth_credentials}"},
+    ) as client:
+        # Check for 400 status code when X-Experience-API-Version header is not included
+        response = await client.get(
+            "/xAPI/statements/",
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "Missing X-Experience-API-Version header"}
+        assert "X-Experience-API-Version" in response.headers
+        assert response.headers["X-Experience-API-Version"] == "1.0.3"
+
+        # Check that X-Experience-API-Version header is included in response
+        response = await client.get(
+            "/xAPI/statements/",
+            headers={"X-Experience-API-Version": "1.0.3"},
+        )
+
+        assert response.status_code == 200
+        assert "X-Experience-API-Version" in response.headers
+        assert response.headers["X-Experience-API-Version"] == "1.0.3"
+
+        # Check for 400 status code when X-Experience-API-Version header
+        # is included but unsupported
+        response = await client.get(
+            "/xAPI/statements/",
+            headers={"X-Experience-API-Version": "1.0.4"},
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "Invalid X-Experience-API-Version header"}
+        assert "X-Experience-API-Version" in response.headers
+        assert response.headers["X-Experience-API-Version"] == "1.0.3"
