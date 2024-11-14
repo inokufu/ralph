bd735b6370b1b499930d51d548fc120cb1ef904c
Thomas Fernandez <thomas.fernandez@inokufu.com>
2024-11-14 15:08:55 +0000

    🔨(backends) add cozy-stack service in docker-compose for testing

---
+++ cozy-config/gentoken.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# $1: instance name
+# $2: sleep time
+
+# generate a new auth token for $1 instance every $2 seconds
+while true
+do
+    cozy-stack instance token-cli $1 io.cozy.learningrecord io.cozy.doctypes > /etc/cozy/cozy-auth-token
+    sleep $2
+done
