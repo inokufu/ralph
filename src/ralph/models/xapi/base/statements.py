16944920fa1dda3075c01398f70b9af7e21a2041
Thomas Fernandez <thomas.fernandez@inokufu.com>
2025-01-08 17:00:25 +0000

    ✅ Check that Object of a voiding Statement is a StatementRef

--- src/ralph/models/xapi/base/statements.py
+++ src/ralph/models/xapi/base/statements.py
@@ -2,7 +2,7 @@
 
 from collections.abc import Mapping, Sequence
 from datetime import datetime
-from typing import Annotated, Any
+from typing import Annotated, Any, Self
 from uuid import UUID
 
 from pydantic import Field, StringConstraints, model_validator
@@ -15,6 +15,8 @@ from .groups import BaseXapiGroup
 from .objects import BaseXapiObject
 from .results import BaseXapiResult
 from .verbs import BaseXapiVerb
+
+VOIDED_VERB_ID = "http://adlnet.gov/expapi/verbs/voided"
 
 
 class BaseXapiStatement(BaseModelWithConfig):
@@ -84,3 +86,14 @@ class BaseXapiStatement(BaseModelWithConfig):
                 )
 
         return values
+
+    @model_validator(mode="after")
+    def check_voiding_requirements(self) -> Self:
+        """Check if a voiding statement is valid."""
+        if (
+            str(self.verb.id) == VOIDED_VERB_ID
+            and self.object.objectType != "StatementRef"
+        ):
+            raise ValueError("Statement verb voided does not use object 'StatementRef'")
+
+        return self
